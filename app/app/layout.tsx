2f8c162970121a6c0ef150a9e98cda392c37d83b
happysuperstar <happysuperstar601@gmail.com>
2024-10-07 11:05:12 -0400

    fix: optimized

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,50 +1,19 @@
-"use client";
-
-import { PrelineScript } from "./components";
-import Script from "next/script";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import "./globals.css";
-
-import ToastProvider from "@/contexts/toastContext";
-import AuthProvider from "@/contexts/AuthProvider";
-import LandingHeaderSection from "./components/landing/Header";
-import { usePathname } from "next/navigation";
-
-const ToastContainerConfig = {
-  closeOnClick: true,
-  draggable: true,
-  pauseOnFocusLoss: true,
-  pauseOnHover: true,
-  autoClose: 3000,
-  hideProgressBar: true,
-  newestOnTop: false,
-  position: "top-right" as const,
-  theme: "colored" as const,
-};
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header"; // Adjust the import path
+// import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const isOnboardingPage = pathname === "/onboarding";
-
   return (
-    <html lang="en">
-      <body className="text-black">
-        <AuthProvider>
-          <ToastProvider>
-            <ToastContainer {...ToastContainerConfig} />
-            <div className="flex h-full min-h-screen flex-col items-center justify-between bg-white">
-              {!isOnboardingPage && <LandingHeaderSection />}
-              {children}
-            </div>
-          </ToastProvider>
-        </AuthProvider>
-      </body>
-      <PrelineScript />
-    </html>
+    <div className="w-screen relative h-screen flex-col flex">
+      <Sidebar />
+
+      <Header />
+      <div className="flex overflow-y-auto flex-1 text-black">{children}</div>
+    </div>
   );
 }
